5466799c039a6cc66872b7b9466a839a90991c1e
Ruth <rwangare124@gmail.com>
2023-04-10 14:54:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,23 +1,125 @@
-/* Your Code Here */
+function createEmployeeRecord(employeeArray) {
+    return {
+      firstName: employeeArray[0],
+      familyName: employeeArray[1],
+      title: employeeArray[2],
+      payPerHour: employeeArray[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+  function createEmployeeRecords(employeesArray) {
+    return employeesArray.map(createEmployeeRecord);
+  }
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+  function createTimeInEvent(employeeRecord, dateTimeString) {
+    const [date, hour] = dateTimeString.split(" ");
+    employeeRecord.timeInEvents.push({
+      type: "TimeIn",
+      date,
+      hour: parseInt(hour, 10)
+    });
+    return employeeRecord;
+  }
 
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+  function createTimeOutEvent(employeeRecord, dateTimeString) {
+    const [date, hour] = dateTimeString.split(" ");
+    employeeRecord.timeOutEvents.push({
+      type: "TimeOut",
+      date,
+      hour: parseInt(hour, 10)
+    });
+    return employeeRecord;
+  }
 
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+  function hoursWorkedOnDate(employeeRecord, date) {
+    const timeIn = employeeRecord.timeInEvents.find(
+      event => event.date === date
+    );
+    const timeOut = employeeRecord.timeOutEvents.find(
+      event => event.date === date
+    );
+    return (timeOut.hour - timeIn.hour) / 100;
+  }
 
-    return payable
-}
+  function wagesEarnedOnDate(employeeRecord, date) {
+    const hoursWorked = hoursWorkedOnDate(employeeRecord, date);
+    return hoursWorked * employeeRecord.payPerHour;
+  }
+
+  function allWagesFor(employeeRecord) {
+    const datesWorked = employeeRecord.timeInEvents.map(event => event.date);
+    return datesWorked.reduce(
+      (totalWages, date) => totalWages + wagesEarnedOnDate(employeeRecord, date),
+      0
+    );
+  }
+
+  function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce(
+      (totalPayroll, employeeRecord) => totalPayroll + allWagesFor(employeeRecord),
+      0
+    );
+  }
+
+  const employeeArray = ["John", "Doe", "Manager", 25];
+  const employeeRecord = createEmployeeRecord(employeeArray);
+
+  console.assert(
+    employeeRecord.firstName === "John",
+    "First name should be John"
+  );
+
+  console.assert(
+    employeeRecord.familyName === "Doe",
+    "Family name should be Doe"
+  );
+
+  console.assert(
+    employeeRecord.title === "Manager",
+    "Title should be Manager"
+  );
+
+  const employeeRecords = [employeeRecord];
+
+  createTimeInEvent(employeeRecord, "2023-04-01 09:00");
+  createTimeOutEvent(employeeRecord, "2023-04-01 17:00");
+
+  console.assert(
+    hoursWorkedOnDate(employeeRecord, "2023-04-01") === 8,
+    "Hours worked on 2023-04-01 should be 8"
+  );
+
+  console.assert(
+    wagesEarnedOnDate(employeeRecord, "2023-04-01") === 200,
+    "Wages earned on 2023-04-01 should be 200"
+  );
+
+  console.assert(
+    allWagesFor(employeeRecord) === 200,
+    "Total wages earned should be 200"
+  );
+
+  const payroll = calculatePayroll(employeeRecords);
+
+  console.assert(
+    payroll === 200,
+    "Total payroll should be 200"
+  );
+
+  function createTimeInEvent(dateStamp) {
+    const [date, time] = dateStamp.split(' ');
+    const [hour, minute] = time.split(':');
+
+    const timeInEvent = {
+      type: 'TimeIn',
+      hour: parseInt(hour),
+      date: date,
+    };
+
+    this.timeInEvents.push(timeInEvent);
+    return timeInEvent;
+  }
+  createTimeInEvent.call(employeeObject, "2023-04-10 14:30");
 
